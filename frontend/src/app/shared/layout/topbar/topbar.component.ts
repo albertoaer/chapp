46680fd703c817650c9a778b988f6a25f074ecd5
Alberto Elorza <albertoelorza2002@gmail.com>
2022-11-05 18:48:31 +0100

    improved topbar

--- frontend/src/app/shared/layout/topbar/topbar.component.ts
+++ frontend/src/app/shared/layout/topbar/topbar.component.ts
@@ -1,9 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-topbar',
   templateUrl: './topbar.component.html',
   styleUrls: ['./topbar.component.css']
 })
-export class TopbarComponent {
+export class TopbarComponent implements OnInit {
+  @Input('currentGroup')
+  currentGroup: string = '';
+
+  enableGroupName: boolean = true;
+
+  ngOnInit(): void {
+    const mediaMatchEvent = (ev: { matches: boolean }) => {
+      this.enableGroupName = !ev.matches;
+    };
+    const mmatch = window.matchMedia("(max-width: 700px)");
+    mediaMatchEvent(mmatch);
+    mmatch.addEventListener("change", mediaMatchEvent);
+  }
 }
