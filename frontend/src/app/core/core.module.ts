c62821a450164eceb879d921dbe3741d548cd3d6
Alberto Elorza <albertoelorza2002@gmail.com>
2022-11-12 01:55:57 +0100

    update core modules

--- frontend/src/app/core/core.module.ts
+++ frontend/src/app/core/core.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { ChatService } from './services/chat.service';
+import { UserService } from './services/user.service';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserModule } from '@angular/platform-browser';
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    HttpClientModule,
+    BrowserModule
+  ],
+  providers: [
+    ChatService,
+    UserService
   ]
 })
 export class CoreModule { }
