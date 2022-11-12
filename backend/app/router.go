17e0a9aff5d1dcdeba1de8a58505b90689961156
Alberto Elorza <albertoelorza2002@gmail.com>
2022-11-12 01:28:31 +0100

    abort without error OPTIONS methods

--- backend/app/router.go
+++ backend/app/router.go
@@ -24,7 +24,12 @@ func Router(
 		ctx.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 		ctx.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
 		ctx.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
-		ctx.Next()
+
+		if ctx.Request.Method == "OPTIONS" {
+			ctx.AbortWithStatus(200)
+		} else {
+			ctx.Next()
+		}
 	})
 
 	// Errors Middleware
